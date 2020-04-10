d0567ee675dd687c0838f4e7aec4ca50bc32d413
freilinjb <freilinjb@gmail.com>
2020-04-09 21:04:54 -0400

    aplicando diseño al componente Contenedor

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-const Frase = () => {
+const ContenedorFrase = styled.div`
+    padding:3rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width:800px;
+    /* media query */
+    @media(min-width: 992px) {
+        margin-top:10rem;
+    }
+    /* styled referenia a sus hijos */
+    h1{
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        padding-left: 4rem;
+
+        /* en sas se transforma al selector de h1 */
+        &::before {
+            content: open-quote; 
+            font-size: 10rem;
+            color:black;
+            position:absolute;
+            left: -1rem;
+            top:-2rem;
+        }
+    }
+
+    p{
+
+    }
+`;
+
+const Frase = ({frase}) => {
     return ( 
-        <h1>FRASE</h1>
+    <ContenedorFrase>
+        <div>
+            <h1><strong>{frase.quote}</strong></h1>
+            <p>{frase.author}</p>
+        </div>
+    </ContenedorFrase>
      );
 }
  
